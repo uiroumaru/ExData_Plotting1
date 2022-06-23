4da1186c589c2aec214536ba9106ec848d7b8fd8
uiroumaru <cgkg3019@mail2.doshisha.ac.jp>
2022-06-23 18:49:37 +0900

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,5 @@
+power <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+power_sub <- subset(power,power$Date=="1/2/2007"|power$Date =="2/2/2007")
+hist(as.numeric(as.character(power_sub$Global_active_power)),col="red",main = "Grobal Active power",xlab = "Grobal Active Power(kilowatts)")
